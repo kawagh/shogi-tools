ce8cbb0b96fb2ad86b5e17e041246d66571acac8
kawagh <kawagh.dev@gmail.com>
2022-10-09 21:17:59 +0900

    generate svg after move #1

--- sfen2svg.py
+++ sfen2svg.py
@@ -1,5 +1,7 @@
+from kif_parser import parse_move
 from sfen_converter import convert
 from svg_board import write_svg
+from move import Move
 
 
 def main():
@@ -7,6 +9,12 @@ def main():
     initial_sfen = "lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL"
     board = convert(initial_sfen)
     with open("from_sfen.svg", "w") as f:
+        write_svg(f, board)
+
+    move: Move = parse_move("７六歩(77)")
+    board.process_move(move)
+
+    with open("from_sfen_after_move.svg", "w") as f:
         write_svg(f, board)
 
 
