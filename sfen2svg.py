01915d05cb5b4031b1d436c353ae3fc3478212f8
kawagh <kawagh.dev@gmail.com>
2022-10-10 14:56:14 +0900

    generated svg files

--- sfen2svg.py
+++ sfen2svg.py
@@ -1,28 +1,31 @@
-from kif_parser import KifParser, parse_move
+from kif_parser import KifParser
 from sfen_converter import convert
 from svg_board import write_svg
 import subprocess
-from move import Move
+import pathlib
 
 
 def main():
 
     initial_sfen = "lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL"
     board = convert(initial_sfen)
-    with open("from_sfen.svg", "w") as f:
+    save_dir = pathlib.Path(__file__).parent / "svgs"
+    save_dir.mkdir(exist_ok=True)
+
+    with open(save_dir / "from_sfen000.svg", "w") as f:
         write_svg(f, board)
 
-    subprocess.run(["eog", "from_sfen.svg"])
+    show_svg = False
 
-    # move: Move = parse_move("７六歩(77)")
     kif_parser = KifParser("./kifs/a.kif")
     kif_parser.parse()
-    for move in kif_parser.moves:
+    for i, move in enumerate(kif_parser.moves, 1):
         print(move)
         board.process_move(move)
-        with open("from_sfen.svg", "w") as f:
+        with open(save_dir / f"from_sfen{i:03}.svg", "w") as f:
             write_svg(f, board)
-        subprocess.run(["eog", "from_sfen.svg"])
+        if show_svg:
+            subprocess.run(["eog", "from_sfen.svg"])
 
 
 if __name__ == "__main__":
