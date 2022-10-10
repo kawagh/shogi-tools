833776a7005641d77afeaeb48a6e190f53d9a8ee
kawagh <kawagh.dev@gmail.com>
2022-10-10 17:50:39 +0900

    refactoring initialize_grid()

--- board.py
+++ board.py
@@ -34,3 +34,48 @@ class Board:
             self.komadai[converted_removed_piece] += 1
 
         self.grid[move.to_row][move.to_col] = piece_type
+
+
+def initialize_grid() -> List[List[Piece]]:
+    _grid = [[Piece.EMPTY] * 9 for _ in range(9)]
+
+    for i in range(9):
+        for j in range(9):
+            if i == 0:
+                if j == 0 or j == 8:
+                    _grid[i][j] = Piece.ENEMY_LANCE
+                if j == 1 or j == 7:
+                    _grid[i][j] = Piece.ENEMY_KNIGHT
+                if j == 2 or j == 6:
+                    _grid[i][j] = Piece.ENEMY_SILVER
+                if j == 3 or j == 5:
+                    _grid[i][j] = Piece.ENEMY_GOLD
+                if j == 4:
+                    _grid[i][j] = Piece.ENEMY_KING
+            if i == 1:
+                if j == 1:
+                    _grid[i][j] = Piece.ENEMY_ROOK
+                if j == 7:
+                    _grid[i][j] = Piece.ENEMY_BISHOP
+
+            if i == 2:
+                _grid[i][j] = Piece.ENEMY_PAWN
+            if i == 6:
+                _grid[i][j] = Piece.PAWN
+            if i == 7:
+                if j == 1:
+                    _grid[i][j] = Piece.BISHOP
+                if j == 7:
+                    _grid[i][j] = Piece.ROOK
+            if i == 8:
+                if j == 0 or j == 8:
+                    _grid[i][j] = Piece.LANCE
+                if j == 1 or j == 7:
+                    _grid[i][j] = Piece.KNIGHT
+                if j == 2 or j == 6:
+                    _grid[i][j] = Piece.SILVER
+                if j == 3 or j == 5:
+                    _grid[i][j] = Piece.GOLD
+                if j == 4:
+                    _grid[i][j] = Piece.KING
+    return _grid
